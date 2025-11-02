9c791b550f9bbd9b03fb00026ab2bc96411f6533
Wisenk <wisenkmr93@gmail.com>
2025-11-02 19:41:40 +0900

    js_learn basic

---
+++ Javascript prog.js
@@ -0,0 +1,36 @@
+	alert ("hey what's up?");
+		document.write("This is our javascript being written first before anything else");
+		
+		console.log ("this is text that will be sent directly to the console to let us know that our program completed successfully");
+		
+		
+		var lives = 3;
+		var example = "I am a variable";
+		document.write (example);
+		
+		example = "This is another example for the same variable";
+		
+		document.write (example);
+		
+		
+		alert ("WELCOME!!");
+		
+		var VisitorName = prompt ("what is your name?");
+		var FavColour = prompt ("what is your favourite colour?");
+		
+		document.write ("Hello " + VisitorName + "Your Favourite Colour is " + FavColour);
+		
+		console.log ("program completed!");
+		
+		
+		
+		
+	var VisitorAge = prompt ("How many years have you been alive for?");
+	
+	var YearsAlive = parseInt (VisitorAge);
+	
+	YearsAlive = YearsAlive * 365
+	
+	document.write ("you have been alive for " + YearsAlive + " days");
+	
+		
