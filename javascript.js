64aa7a12a3bcaa3ab704fd27b9495f47a70dee40
Wisenk <wisenkmr93@gmail.com>
2025-11-04 00:22:55 +0900

    odin project_js node install

---
+++ javascript.js
@@ -0,0 +1,38 @@
+let name = "John";
+let surname = "Doe";
+
+console.log (name);
+console.log (surname);
+
+
+const pi = (3 + 2) - 76* (1+1);
+
+console.log (pi);
+
+console.log ((5 + 6) + 52 * (5 - 2));
+
+console.log(4 + 6 + 9) / 77;
+
+let a = 25;
+let b = 7 * a;
+console.log (b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log (percentage);
+
+
+let c= "hello ";
+let d = "nice to meet you";
+console.log (c + d);
+
+let x = 10;
+let y = 20;
+let z = "The result is: " + x + y;
+console.log (z);
+
+const myInt = 5;
+const myFloat = 6.667;
+console.log (typeof myInt);
+console.log (typeof myFloat);
